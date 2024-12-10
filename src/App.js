531ad69fbcc00505e2acd2763a72c3b853fa9b9a
mamatha12-reddy <mamathareddy3456.reddeppa@gmail.com>
2024-12-10 12:58:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+
+function App() {
+  const [advice,setAdvice]=useState('')
+  const [loading,setLoading]=useState(false)
+  useEffect(()=>{
+    console.log("the data comg==")
+    
+    
+  },[])
+  const fetchData=async()=>{
+    console.log("Function calling=*******************")
+    const res= await fetch('https://api.adviceslip.com/advice')
+    const data= await res.json()
+    
+    setAdvice(data.slip.advice)
+    setLoading(true)
+  }
+  return (
+    <div className="app">
+      <div className="card">
+        {
+          loading?
+          <h1 className='heading'>{advice}</h1>
+          :<h1>Loading data</h1>
+        }
+      <button className='button' onClick={()=>fetchData()}>
+        <span>GIVE ME ADVICE!</span>
+      </button>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
